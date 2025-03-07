3b742a099ee8812ad89a4c9b79107ab16709e4ab
jaypat12 <jaypat@udel.edu>
2025-03-06 22:56:50 -0500

    there we go

--- Array-Hashing/Easy/Find-The-Prefix-Commom-Array-2657.py
+++ Array-Hashing/Easy/Find-The-Prefix-Commom-Array-2657.py
@@ -14,21 +14,40 @@ def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
             if A[i] not in d:
                 d[A[i]] = 1
             else:
-                d[A[i]] += 1
                 count += 1
             if B[i] not in d:
                 d[B[i]] = 1
             else:
-                d[B[i]] += 1
                 count +=1
             
             arr.append(count)
         
         return arr
 
+
+def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
+        seen = set()  # To track seen elements
+        count = 0  # Count of common elements
+        result = []  # Final array
+
+        for i in range(len(A)):
+            if A[i] in seen:  # If already seen, it's a repeat, increment count
+                count += 1
+            seen.add(A[i])  # Mark A[i] as seen
+            
+            if B[i] in seen:  # If already seen (from A or before in B), increment count
+                count += 1
+            seen.add(B[i])  # Mark B[i] as seen
+
+            result.append(count)  # Store current count
+
+        return result
+
+
 '''
 
-Can you please redo this! I dont really understand so we are goin to look at this again
+The way this works is that if the if index 0 of both arrays is not in the dict, you add it
+but if it is, then you simply increase the count by 1. this works because there is no duplicates
 
 '''
             
--- Array-Hashing/Easy/Two-Out-of-Three-2032.py
+++ Array-Hashing/Easy/Two-Out-of-Three-2032.py
@@ -15,4 +15,59 @@ def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) ->
 
         return arr
 
-"finish commenting"
+'''
+
+Basically the goal was to see if there are numbers within the arrays that are present
+within 2 out of three array. So what I do is get the max number amongst all the arrays
+Form there, i did did a range from 0 to the highest number. For every single number i checked
+if that number belonged to 2 of the following arrays. So (1,2) or (2,3) or (1,3). If it was there
+I added to arry
+
+'''
+
+
+def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
+        arr = []
+        a = set(nums1)
+        b = set(nums2)
+        c = set(nums3)
+
+        d = list(a) + list(b) + list(c)
+
+        di = {}
+        for i in d:
+            if i not in di:
+                di[i] = 1
+            else:
+                if i not in arr:
+                    arr.append(i)
+        
+        return arr
+
+
+'''
+
+For this one, what I did is get the set of each array. this would remove any duplicated in
+the array. And from there I added the sets together by making them a array. So then,
+from there I put ino tthe dictionary, if the integer is not in dict, then we place it and record
+the frequecny. Now if the integer is the dictionary but not in the arr. Then we append into array
+meaning it would be final answer.
+
+a = [1,1,3,2]
+b = [2,3]
+c = [3]
+
+after set
+
+{1,3,2} {2,3} {3}
+
+not cover into list and add them together
+
+[1,3,2,2,3,3]
+
+then put in dict, and if it appeared again check if in arr, if not then
+we pust that answer in the arr.
+
+so [2,3] is th answer
+
+'''
